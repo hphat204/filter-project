86e0a8aa682fa29a64c29b45f6a1e5c951d6bf51
hphat204 <pphat620@gmail.com>
2021-04-05 13:46:19 +0700

    added Cart feature

--- scripts.js
+++ scripts.js
@@ -8,10 +8,11 @@ const itemArr = document.getElementsByClassName("item");
 const modal = document.querySelector(".modal");
 const modelContent = document.querySelector(".modal-content");
 const btnClose = document.querySelector(".close");
+
 const all = [
   {
     id: "chicken",
-    ten: "chicken-3",
+    ten: "chicken-1",
     price: 25 + "$",
     img: "pic/chicken/download.jpeg",
   },
@@ -68,6 +69,7 @@ function addItem(item) {
   return `<div class="item">
 <div class="card-item">
 <img src=${item.img} class="card-img">
+<i class="far fa-plus-square"></i>
 <div class="card-info">
 <p>${item.ten}</p>
 <p>${item.price}</p>
@@ -77,38 +79,41 @@ function addItem(item) {
 }
 all.forEach((item) => {
   items.insertAdjacentHTML("afterbegin", addItem(item));
-  modalItems();
 });
 btnAll.addEventListener("click", function () {
   items.textContent = "";
   all.forEach((item) => {
     items.insertAdjacentHTML("afterbegin", addItem(item));
-    modalItems();
   });
+  modalItems();
+  buyActive();
 });
 btnChicken.addEventListener("click", function () {
   items.textContent = "";
   const chickenfilter = all.filter((chick) => chick.id === "chicken");
   chickenfilter.forEach((item) => {
     items.insertAdjacentHTML("afterbegin", addItem(item));
-    modalItems();
   });
+  modalItems();
+  buyActive();
 });
 btnPasta.addEventListener("click", function () {
   items.textContent = "";
   const pastafilter = all.filter((pas) => pas.id === "pasta");
   pastafilter.forEach((item) => {
     items.insertAdjacentHTML("afterbegin", addItem(item));
-    modalItems();
   });
+  modalItems();
+  buyActive();
 });
 btnPizza.addEventListener("click", function () {
   items.textContent = "";
   const pizzafilter = all.filter((pi) => pi.id === "pizza");
   pizzafilter.forEach((item) => {
     items.insertAdjacentHTML("afterbegin", addItem(item));
-    modalItems();
   });
+  modalItems();
+  buyActive();
 });
 inputbox.addEventListener("keyup", function () {
   items.textContent = "";
@@ -122,14 +127,16 @@ inputbox.addEventListener("keyup", function () {
   } else {
     filterFood.forEach((item) => {
       items.insertAdjacentHTML("afterbegin", addItem(item));
-      modalItems();
     });
+    modalItems();
+    buyActive();
   }
 });
 function modalItems() {
-  let itemsArr = [...itemArr];
+  const itemsArr = [...itemArr];
   itemsArr.forEach((item) => {
-    item.addEventListener("click", function () {
+    item.addEventListener("click", function (e) {
+      if (e.target.className === "far fa-plus-square") return;
       modal.style.display = "block";
       const imgurl = this.querySelector("img").src;
       const imgUrl = imgurl.slice(21);
@@ -147,4 +154,52 @@ modal.addEventListener("click", function (e) {
   if (e.target === modal) {
     modal.style.display = "none";
   }
+});
+
+const cartItem = document.querySelector(".cart-item");
+let cartItems = [];
+
+function buyActive() {
+  const btnBuyArr = document.querySelectorAll(".fa-plus-square");
+  const btnBuy = [...btnBuyArr];
+  btnBuy.forEach((btn) => {
+    btn.addEventListener("click", function () {
+      const itemName = [
+        this.parentNode.textContent.trim().slice(0, -4),
+      ].toString();
+      const chooseItem = all.filter((item) => itemName === item.ten);
+      cartItems.push(...chooseItem);
+      const price = cartItems
+        .map((item) => Number(item.price.slice(0, -1)))
+        .reduce((acc, cur) => acc + cur);
+      console.log(price);
+      document.querySelector(".total").textContent = `total : ${price}$`;
+      cartItem.insertAdjacentHTML("afterbegin", addedItem(chooseItem[0]));
+    });
+  });
+}
+buyActive();
+
+function addedItem(item) {
+  return `
+
+    <div class='item-list'>
+      <img src=${item.img} >
+      <p class="item-name">${item.ten}</p>
+      <p class="item-price">${item.price}</p>
+    </div>  
+  `;
+}
+const cart = document.querySelector(".cart");
+const totalPrice = document.querySelector(".total-price");
+cart.addEventListener("click", function () {
+  cartItem.classList.toggle("nodisplay");
+  totalPrice.classList.toggle("nodisplay");
+});
+const btnClear = document.querySelector(".clear");
+btnClear.addEventListener("click", function () {
+  cartItems = [];
+  cartItem.textContent = "";
+  document.querySelector(".total").textContent = "total : 0";
+  console.log(cartItems);
 });
